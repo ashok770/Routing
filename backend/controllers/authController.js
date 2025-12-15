8f8cd95d138693689dbe77287d5142bd41ca46ac
ashok770 <ashoktamata6002@gmail.com>
2025-12-15 20:21:50 +0000

    mongodb connected and the registration part is done and checked in the postman it work

---
+++ backend/controllers/authController.js
@@ -0,0 +1,32 @@
+const User = require("../models/User");
+
+// @desc Register user
+// @route POST /api/auth/register
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,15 @@
 const express = require("express");
-const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
-const PORT = 5000;
+app.use(express.json()); // VERY IMPORTANT
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("CookMate Backend is running 🚀");
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
