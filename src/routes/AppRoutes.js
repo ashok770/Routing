2ee24158bff99f2e9e4fce026132382c7c543282
ashok770 <ashoktamata6002@gmail.com>
2025-12-17 22:23:53 +0000

    authentication done okay

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,34 +1,42 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import BlogDetail from "../pages/BlogDetail";
-import Register from "../pages/Register";
 
 import Home from "../pages/Home";
 import Recipes from "../pages/Recipes/Recipes";
 import RecipeDetail from "../pages/RecipeDetail";
 import CookingGuides from "../pages/CookingGuides";
 import GuideDetail from "../pages/GuideDetail";
-
 import Blog from "../pages/Blog";
+import BlogDetail from "../pages/BlogDetail";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 import Profile from "../pages/Profile";
+
+import ProtectedRoute from "./ProtectedRoute";
 
 const AppRoutes = () => {
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/" element={<Home />} />
       <Route path="/recipes" element={<Recipes />} />
-
-      {/* ⭐ IMPORTANT ROUTE FOR DYNAMIC RECIPE */}
       <Route path="/recipe/:id" element={<RecipeDetail />} />
-      <Route path="/register" element={<Register />} />
       <Route path="/guides" element={<CookingGuides />} />
       <Route path="/guide/:id" element={<GuideDetail />} />
-      <Route path="/blog/:id" element={<BlogDetail />} />
-      <Route path="/profile" element={<Profile />} />
       <Route path="/blog" element={<Blog />} />
+      <Route path="/blog/:id" element={<BlogDetail />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* 🔐 Protected Route */}
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
